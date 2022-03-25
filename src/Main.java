90bcec727aaa2dae0d302dff4715416ebcfe8494
DiegoAleksis <diegoarh19@gmail.com>
2022-03-25 11:50:59 -0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+    Calculadora objetobasica=new Calculadora("Patito", false);
+    Calculadora objetoCientifica= new Calculadora("Casio",true);
+
+    }
 }
