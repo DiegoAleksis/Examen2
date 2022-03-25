16581a47232a4a47cb65273bf9f6f02a51330cf4
DiegoAleksis <diegoarh19@gmail.com>
2022-03-25 11:44:12 -0600

    Main

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,5 @@
+import com.sun.source.doctree.SummaryTree;
+
 public class Calculadora {
     private String marca;
     private boolean esCientifica;
@@ -41,5 +43,36 @@ public class Calculadora {
     public Calculadora(String marca, boolean esCientifica) {
         this.marca = marca;
         this.esCientifica = esCientifica;
+    }
+    public double sumar (){
+        return primerValor+segundoValor;
+    }
+    public double resta (){
+        return primerValor-segundoValor;
+    }
+    public double multiplica (){
+        return primerValor*segundoValor;
+    }
+    private double dividir () {
+            if (primerValor / segundoValor == 0) {
+            return 0.0;}
+            else {
+                return dividir();
+            }
+
+
+        }
+        public String elevarAPotencia (){
+        if (esCientifica==true) {
+            return "" +Math.pow(primerValor, segundoValor);
+        }else {
+            return "no es cientifica";
+        }
+
+        }
+
+    @Override
+    public String toString() {
+        return "La Calculadora es de marca" +marca+ "y" +esCientifica;
     }
 }
